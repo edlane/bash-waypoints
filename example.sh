12118804dc29da7a74c3e71b1904dbb44cf22a90
ed lane <ed.lane.0@gmail.com>
2019-11-15 17:02:09 -0700

    more/better examples

--- example.sh
+++ example.sh
@@ -2,12 +2,16 @@
 
 source ./waypoint.source
 
+# ------------ a waypoint which requires escaped double quotes...
 waypoint echo "\">>>>>>>>>>>> Test for waypoints <<<<<<<<<<<<\""
+
+
+# ------------ a waypoint which with options...
 waypoint uname -a
-waypoint pwd
 
 waypoint echo $*
 
+# ------------ a waypoint interspersed with "non-waypoints"...
 waypoint date
 date
 date
@@ -17,6 +21,22 @@ date
 waypoint date
 
 
+# ------------ a waypoint inside a bash loop...
+counter=1
+until [ $counter -gt 10 ]
+do
+  waypoint echo counter = $counter
+  ((counter++))
+done
+
+
+# ------------ more waypoints...
 waypoint whoami
 waypoint id
+waypoint pwd
+
+# ------------ more waypoints with environment variables...
+waypoint echo my pid is $$
+waypoint echo my home is $HOME
+waypoint echo this script has been running for $SECONDS seconds
 
